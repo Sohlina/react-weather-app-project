5df9f0041c81f12ac2496887dd43f84ef98b48c0
Sohlina <a.sohlin@hotmail.com>
2021-05-22 15:02:14 +0200

    Added CSS styling for Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -10,19 +10,31 @@ export default function Weather() {
         <input 
           type="search" 
           placeholder="Enter a city.." 
-          className="form-control" />
+          className="form-control" 
+          autoFocus="on" />
           </div>
           <div className="col-3">
-        <button type="button" className="btn btn-dark">Enter</button>
+        <button 
+          type="button" 
+          className="btn btn-dark w-100">Enter</button>
       </div>
       </div>
       </form>
-      <h2>Queenstown</h2>
+      <h1>Queenstown</h1>
+      <p>Sunny</p>
        Monday ⎜ May 22 ⎜ 11:30
-      <div className="row">
+      <div className="row mt-5">
         <div className="col-6">
-         <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny" />
-          2°C
+          <div className="clearfix">
+             <img 
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" 
+              alt="Sunny"
+              className="float-left"
+            />
+              <span className="temperature">2</span>
+              <span className="unit">°C</span>
+           
+          </div>
         </div>
         <div className="col-6">
          <ul>
