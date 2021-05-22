27f49a49013bd92a29b62e92c0c0529b8a5fea01
Sohlina <a.sohlin@hotmail.com>
2021-05-22 09:38:54 +0200

    Added Github repository URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Weather App</h1>
+      <footer>
+      <a href="https://github.com/Sohlina/react-weather-app-project" target="_blank">Open-source code</a><small>, by Alexandra Sohlin</small>
+    </footer>
     </div>
   );
 }
