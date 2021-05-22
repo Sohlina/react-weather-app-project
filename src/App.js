146b6efb6c9406b9c9c387da75c52f9196faa1f5
Sohlina <a.sohlin@hotmail.com>
2021-05-22 09:53:09 +0200

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       <h1>React Weather App</h1>
       <footer>
-      <a href="https://github.com/Sohlina/react-weather-app-project" target="_blank">Open-source code</a><small>, by Alexandra Sohlin</small>
+      <a href="https://github.com/Sohlina/react-weather-app-project" target="_blank" rel="noreferrer">Open-source code</a><small>, by Alexandra Sohlin</small>
     </footer>
     </div>
   );
