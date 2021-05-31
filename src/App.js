04dd1f5ec43b278e370f2fdf0c7f73871bc2513f
Sohlina <a.sohlin@hotmail.com>
2021-05-31 07:25:04 +0200

    Fixed warnings and placed the temperature next to the icon

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
     <div className="App">
       <div className="container">  
       <Weather defaultCity="Queenstown" />
-      <footer>
-      This project is <a href="https://github.com/Sohlina/react-weather-app-project" target="_blank" rel="noreferrer"> Open-sourced on Github </a><a href="https://jovial-aryabhata-bbec83.netlify.app/" target="_blank" rel="noreferrer"></a>, coded by Alexandra Sohlin 👩🏽‍💻
+    <footer>
+      This project is <a href="https://github.com/Sohlina/react-weather-app-project" target="_blank" rel="noreferrer"> Open-sourced on Github</a>, coded by Alexandra Sohlin 👩🏽‍💻
     </footer>
     </div>
   </div>
--- src/CurrentWeatherInfo.js
+++ src/CurrentWeatherInfo.js
@@ -8,22 +8,26 @@ import WeatherUnit from "./WeatherUnit";
 export default function CurrentWeatherInfo(props) {
     return(
       <div className="CurrentWeatherInfo">
-        <h1><i className="fas fa-map-marker-alt"></i> {props.data.city}</h1>
-    <CurrentDate date={props.data.date}/>
-      <p className="text-capitalize"><strong>{props.data.description}</strong></p>
-        <div className="row mt-5">
-          <div className="col-7">
-            <div className="clearfix">
-              <WeatherIcon 
-                code={props.data.icon} 
-                alt={props.data.description}
-                size={60}
-              />
-          </div>
-        <WeatherUnit celsius={props.data.temperature}/>
-        </div>
-    <div className="col-5">
-      <ul>
+       <h1><i className="fas fa-map-marker-alt"></i> {props.data.city}</h1>
+        <CurrentDate date={props.data.date}/>
+          <p className="text-capitalize"><strong>{props.data.description}</strong></p>
+            <div className="row mt-5">
+              <div className="col-7">
+                <div className="d-flex flex-row">
+                  <div className="p-2">
+                  <WeatherIcon 
+                    code={props.data.icon} 
+                    alt={props.data.description}
+                    size={64}
+                  />
+                </div>
+                <div className="p-2">
+              <WeatherUnit celsius={props.data.temperature}/>
+              </div>
+            </div>
+            </div>
+          <div className="col-5">
+        <ul>
         <li><i className="fas fa-temperature-high"></i> Max temp: {Math.round(props.data.maxTemperature)} °C </li>
         <li><i className="fas fa-temperature-low"></i> Min temp: {Math.round(props.data.minTemperature)} °C </li>
         <li><i className="fas fa-tint"></i> Humidity: {props.data.humidity} % </li>
